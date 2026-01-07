f1c80a46dc62de44003a3520bae2d70d86a3307d
@7m3D <54592936+Ahmed-Alhaddad@users.noreply.github.com>
2026-01-07 20:49:54 +0200

    Create Copyfiletofolder.sh
    #Copy files to folders that match numbers file name even if the name start with letters

---
+++ Basic/Copyfiletofolder.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for files in *.pdf; do
+dir=$(echo "$files" | grep -o "[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]") # extract first E## from filename
+mkdir -p "$dir"  # create dir
+mv "$files" "$dir"
+done
